da32436fa9b5ef1310f566194eb0bd23f312f6b1
Pramuditha Bimsara <pramudithabimsara@gmail.com>
2026-08-02 10:05:00 +0530

    Show/hide Password added

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+// Get the password input
+const passwordInput = document.getElementById("password");
+
+// Get the button
+const toggleButton = document.getElementById("togglePassword");
+
+// Add click event
+toggleButton.addEventListener("click", function () {
+
+    if (passwordInput.type === "password") {
+
+        passwordInput.type = "text";
+        toggleButton.textContent = "Hide Password";
+
+    } else {
+
+        passwordInput.type = "password";
+        toggleButton.textContent = "👁 Show Password";
+
+    }
+
+});
